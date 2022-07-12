03475a3ed3a8a8a7a854823cb25838a1116c4009
OutrunSun <67318042+OutrunSun@users.noreply.github.com>
2022-07-12 13:59:35 +0100

    initial commit

---
+++ Text Based Hangman/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Text_Based_Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("HANGMAN - C# CONSOLE EDITION");
+            Console.Write("\nBy Noah Ramdhony");
+            Config();
+            Console.Write("\nThanks for playing");
+        }
+
+        static void Config()
+        {
+            Console.Write("\nHow many players: ");
+            int playerCount = Convert.ToInt32(Console.ReadLine());
+
+            string selectedWord = "";
+
+            if (playerCount > 1)
+            {
+                Console.Write("Input a word: ");
+                selectedWord = Console.ReadLine().ToUpper();
+            } 
+            else 
+            {
+                selectedWord = WordGeneration();
+            }
+
+            GameLoop(selectedWord);
+        }
+
+        static string WordGeneration()
+        {
+            // Initialises the Variables needed for random line
+            Random index = new Random();
+            string word = "";
+
+            using (var reader = new StreamReader(@"C:\Users\Njbra\Documents\@ Programming\C#\Text Based Hangman\words.txt"))
+            {
+                // Cycles through all lines before the one selected from the random
+                for (int i = 1; i < index.Next(1, 10000); i++)
+                {
+                    reader.ReadLine();
+                }
+
+                // Sets the selected word to a variable
+                word = reader.ReadLine().ToUpper();
+            }
+            return word;
+        }
+
+        static void GameLoop(string word)
+        {
+            // Defining Variables used
+            int lives = 9;
+            List<char> alphabet = new List<char>();
+            List<string> wordsGuessed = new List<string>();
+            int winStatus = 0;
+
+            do
+            {  
+                Renderer(word, lives, alphabet, wordsGuessed, winStatus);
+                if (lives == 0)
+                {
+                    winStatus = -1;
+                }
+                else
+                {
+                    Console.Write("LIVES:   " + lives);
+
+                    Console.Write("\nInput Guess: ");
+                    string guess = Console.ReadLine().ToUpper();
+                    char selection;
+                    if (guess.Length == 1)
+                    {
+                        selection = 'L';
+                    }
+                    else
+                    {
+                        selection = 'W';
+                    }
+
+                    switch (selection)
+                    {
+                        case 'W':
+                            if (guess == word)
+                            {
+                                winStatus = 1; // 1 = Win, -1 = Loss, 0 = Playing
+                            }
+                            else
+                            {
+                                lives--;
+                                wordsGuessed.Add(guess);
+                            }
+
+                            break;
+
+                        case 'L':
+                            if (word.IndexOf(guess) == -1)
+                            {
+                            lives--;
+                            }
+                            alphabet.Add(Convert.ToChar(guess));
+                            break;
+                    }
+                }
+
+            }while (winStatus == 0);
+            Renderer(word, lives, alphabet, wordsGuessed, winStatus);
+            
+        }
+
+        static void Renderer(string word, int lives, List<char> lettersGuesses, List<string> wordGuesses, int winStatus)
+        {
+            Console.Clear();
+
+            // The Title:
+            Console.Write("HANGMAN - C# Console\n");
+
+            // The Drawing itself
+            if (lives < 8)
+            {
+                Console.Write("    +---+\n");
+            }
+            else
+            {
+                Console.Write("\t\n");
+            }
+            switch (lives)
+            {
+                case 0:
+                    Console.Write("    |   |\n    O   |\n   /|\\  |\n   / \\  |\n        |\n");
+                    break;
+
+                case 1:
+                    Console.Write("    |   |\n    O   |\n   /|\\  |\n   /    |\n        |\n");
+                    break;
+                
+                case 2:
+                    Console.Write("    |   |\n    O   |\n   /|\\  |\n        |\n        |\n");
+                    break;
+                
+                case 3:
+                    Console.Write("    |   |\n    O   |\n   /|   |\n        |\n        |\n");
+                    break;
+
+                case 4:
+                    Console.Write("    |   |\n    O   |\n    |   |\n        |\n        |\n");
+                    break;
+                
+                case 5:
+                    Console.Write("    |   |\n    O   |\n        |\n        |\n        |\n");
+                    break;
+                
+                case 7:
+                    Console.Write("        |\n        |\n        |\n        |\n        |\n");
+                    break;
+                
+                case 6:
+                    Console.Write("    |   |\n        |\n        |\n        |\n        |\n");
+                    break;
+                
+                case 8:
+                    Console.Write("        |\n        |\n        |\n        |\n        |\n");
+                    break;
+                
+                case 9:
+                    Console.Write("\t \t\n\t \t\n\t \t\n\t \t\n\t \t\n");
+                    break;
+            }
+            Console.Write("   ======\n\n ");
+            
+            // The Word display:
+            if (winStatus == 1)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write(word[i] + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (lettersGuesses.Contains(word[i]))
+                    {
+                        Console.Write(word[i] + " ");
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+            }
+            Console.Write("\n");
+
+            // Letters Guessed Display:
+            Console.Write("\nLETTERS: ");
+            foreach (char letter in lettersGuesses)
+            {
+                Console.Write(letter);
+            }
+            Console.Write("\n");
+
+            // Words Guessed Display
+            Console.Write("WORDS:   ");
+            foreach (string currentWord in wordGuesses)
+            {
+                Console.Write(currentWord);
+            }
+            Console.Write("\n");
+
+            // Win Display:
+            switch (winStatus)
+            {
+                case 1:
+                    Console.Write("\n! Y O U   W O N !");
+                    break;
+                
+                case -1:
+                    Console.Write("\n! Y O U   L O S T !");
+                    Console.Write("\nThe word was: " + word);
+                    break;
+            }
+        }
+    }
+}
---
+++ Text Based Minesweeper/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+
+namespace Text_Based_Minesweeper{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+            Setup();
+            Console.Write("Thanks for playing");
+        }
+
+        static void Setup(){
+            Console.WriteLine("MINESWEEPER - C# CONSOLE EDITION");
+            Console.WriteLine("By Noah Ramdhony");
+
+            // Width Input:
+            Console.Write("Input Width (MAX: 25): ");
+            int width = Convert.ToInt32(Console.ReadLine());
+                
+            
+            // Height Input
+            Console.Write("Input Height(MAX: 10): ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            
+            // Difficulty Input
+            Console.Write("Input Difficulty(1 to 5): ");
+            int difficulty = (6 - (Convert.ToInt32(Console.ReadLine()))) * 2;
+            // Passes values to the Generator
+            Generator(width, height, difficulty);
+        }
+
+        static void Generator(int w = 5, int h = 5, int difficulty = 2){
+            // Defines the total bombs in the area as an int
+            int totalBombs = (w * h) / difficulty;
+
+            // Defines an array with volume specified in Setup()
+            int[,] hiddenGrid = new int[w, h];
+
+            // Defines an array that the user sees
+            char[,] displayedGrid = new char[w, h];
+
+            // Adds the 'bombs' to the hiddenGrid - indicated by a 9 Value
+            Random Coord = new Random();
+
+            //Random yCoord = new Random();
+            int x, y = 0;
+            for (int i = totalBombs; i > -1; i--){
+                x = Coord.Next(0, w);
+                y = Coord.Next(0, h);
+                if (hiddenGrid[x, y] != 9){
+                    hiddenGrid[x, y] = 9;
+                }
+                else{
+                    i++;
+                };
+            }
+
+            // Assigns Values to both grids
+            // Counts the occurances of bombs
+            int counter = 0;
+            for (int x2 = 0; x2 < w; x2++){
+                for (int y2 = 0; y2 < h; y2++){
+                    
+                    // Sets all values int he visible grid to '?'
+                    displayedGrid[x2, y2] = '?';
+
+                    // Skips all bombs in hidden Grid
+                    if (hiddenGrid[x2, y2] != 9){
+                        counter = 0;
+                        for (int modifierX = -1; modifierX < 2; modifierX++){
+                            for (int modifierY = -1; modifierY < 2; modifierY++){
+                                try{
+                                    // Counts how many adjacent blocks have a bomb
+                                    if (hiddenGrid[(x2 + modifierX), (y2 + modifierY)] == 9)
+                                    {
+                                        counter++;
+                                    }
+                                }
+                                // Skips attempts outside of the bounds
+                                catch (System.IndexOutOfRangeException){}
+                            }
+                        }
+                        // Sets the block to a value of how many bombs adjacent
+                        hiddenGrid[x2, y2] = counter;
+                    }
+                }
+            }
+            // Passes control over to the GameLoop
+            GameLoop(w, h, displayedGrid, hiddenGrid, totalBombs);
+        }
+
+        static void DeveloperRenderer(int w, int h, char[,] displayedGrid, int[,] hiddenGrid, int bombTotal, int flagsPlaced, bool loss, int turns){
+            Console.Clear();
+            Console.Write("MINESWEEPER - C# CONSOLE EDITION\n");
+            Console.Write("? = Hidden, ! = Flagged, # = Exploded\nDEVELOPER RENDER\n");
+
+            // Draws the bar of X coordinates
+            string topBar = "% A B C D E F G H I J K L M N O P Q R S T U V W X Y ";
+            Console.Write(topBar.Substring(0, ((w + 1) * 2)));
+
+            // Draws the Displayed Grid itself
+            for (int y = 0; y < (h); y++){
+                Console.Write("\n" + y + " ");
+                for (int x = 0; x < w; x++){
+                    Console.Write(hiddenGrid[x, y] + " ");
+                }
+            }
+
+        }
+        
+
+        static void Renderer(int w, int h, char[,] displayedGrid, int bombTotal, int flagsPlaced, bool loss, int turns){
+            Console.Clear();
+            Console.Write("MINESWEEPER - C# CONSOLE EDITION\n");
+            Console.Write("? = Hidden, ! = Flagged, # = Exploded\n");
+
+            // Draws the bar of X coordinates
+            string topBar = "% A B C D E F G H I J K L M N O P Q R S T U V W X Y ";
+            Console.Write(topBar.Substring(0, ((w + 1) * 2)));
+
+            // Draws the Displayed Grid itself
+            for (int y = 0; y < (h); y++){
+                Console.Write("\n" + y + " ");
+                for (int x = 0; x < w; x++){
+                    Console.Write(displayedGrid[x, y] + " ");
+                }
+            }
+
+            Console.Write("\nFlags Used: " + flagsPlaced + "/" + bombTotal + ", Turns Used: " + turns + "\n");
+
+            if (loss){
+                Console.Write("\nYOU BLEW TO SMITHEREENS - Game Over\n\n");
+            }
+        }
+
+        static void GameLoop(int w, int h, char[,] displayedGrid, int[,] hiddenGrid, int bombTotal){
+            // Establishes the reused variables
+            bool playing = true;
+            bool loss = false;
+            int selectedY, selectedX, turns = 0;
+            int flagsPlaced = 0;
+            char type, selectedXLetter;
+            string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+
+
+            do{
+                // Draws to console
+                Renderer(w, h, displayedGrid, bombTotal, flagsPlaced, loss, turns);
+
+                // X Input
+                Console.Write("\nInput X: ");
+                selectedXLetter = Convert.ToChar((Console.ReadLine()).ToUpper());
+                selectedX = Alphabet.IndexOf(selectedXLetter);
+                
+                // Y Input
+                Console.Write("Input Y: ");
+                selectedY = Convert.ToInt32(Console.ReadLine());
+
+                // Command Type input
+                // F = Flag, U = Unveil, R = Remove
+                Console.Write("Input Type(F = Flag, U = Unveil, R = Remove): ");
+                type = Convert.ToChar((Console.ReadLine()).ToUpper());
+
+                switch (type){
+                    case 'F':
+                        if (hiddenGrid[selectedX, selectedY] != '?'){
+                            displayedGrid[selectedX,selectedY] = '!'; 
+                        }
+                        break;
+
+                    case 'U':
+                        if (hiddenGrid[selectedX, selectedY] == 9){
+                            displayedGrid[selectedX, selectedY] = '#';
+                            loss = true;
+                            Renderer(w, h, displayedGrid, bombTotal, flagsPlaced, loss, turns);
+                            playing = false;
+                        }
+                        else{
+                            displayedGrid[selectedX, selectedY] = Convert.ToChar($"{hiddenGrid[selectedX, selectedY]}");
+                        }
+                        break;
+
+                    case 'R':
+                        if (displayedGrid[selectedX,selectedY] == '!'){
+                            displayedGrid[selectedX, selectedY] = '?';
+                        }
+                        break;
+
+                    case 'D':
+                        DeveloperRenderer(w, h, displayedGrid, hiddenGrid, bombTotal, flagsPlaced, loss, turns);
+                        Console.ReadLine();
+                        break;
+                }
+                turns++;
+            } while (playing);
+        } 
+    }
+}
